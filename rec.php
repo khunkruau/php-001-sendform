fe0a6313299056422c1d96b0129e7e1ce8a76814
khunkruau <137683680+khunkruau@users.noreply.github.com>
2023-07-21 11:01:54 +0700

    update 21-07-2566

--- rec.php
+++ rec.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>รับข้อมูลฟอร์ม</title>
+    <title>รับข้อมูลฟอร์ม.</title>
 </head>
 
 <body>
